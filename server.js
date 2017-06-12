e208cda87c398f1de438750d820024e4dcef5075
Amani Deepthi Matta <amanideepthi9@gmail.com>
2017-06-12 20:51:33 +0530

    Create server.js

--- server.js
+++ server.js
@@ -1,32 +1,6 @@
 var express = require('express');
 var app = express();
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.use(express.static(__dirname + '/public'));
 var port = process.env.PORT || 3000;
-
-// load and configure the server to use cookie based session support.
-var passport      = require('passport');
-var cookieParser  = require('cookie-parser');
-var session       = require('express-session');
-
-app.use(session({
-    secret: 'This is a secret',
-    resave: true,
-    saveUninitialized: true
-}));
-
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
-
-// configure a public directory to host static content
-
-require ("./test/app.js")(app);
-require("./assignment/app.js")(app);
-require("./project/app.js")(app);
 
 app.listen(port);
